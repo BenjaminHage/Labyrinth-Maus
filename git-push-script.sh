15d6e92c8b84e01beff22bbc67a48449e21a47df
BenjaminHage <hagebenjamin@gmail.com>
2024-08-17 12:49:39 +0200

    Update git-push-script.sh

--- git-push-script.sh
+++ git-push-script.sh
@@ -3,9 +3,14 @@
 # Staging aller Änderungen
 git add .
 
-# Abfrage der Commit-Nachricht
-echo "Geben Sie Ihre Commit-Nachricht ein: "
-read commitMessage
+# Eingabe der Commit-Nachricht mit dialog
+commitMessage=$(dialog --inputbox "Geben Sie Ihre Commit-Nachricht ein:" 10 50 3>&1 1>&2 2>&3)
+
+# Überprüfe, ob die Commit-Nachricht leer ist
+if [ -z "$commitMessage" ]; then
+    echo "Commit-Nachricht darf nicht leer sein."
+    exit 1
+fi
 
 # Commit mit der eingegebenen Nachricht
 git commit -m "$commitMessage"
