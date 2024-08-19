dbacab052a9f539e428ce8e749f0db1dc011253f
BenjaminHage <hagebenjamin@gmail.com>
2024-08-19 15:56:31 +0200

    print terminal entfernd

--- maus.py
+++ maus.py
@@ -6,29 +6,6 @@ from controllers import PIDController
 import io_management as io
 from robot import DifferentialDriveRobot as Robot
 
-def print_terminal(robot, left_wheel_velocity, right_wheel_velocity, base_speed,
-                   angle_setpoint, angle_control, pid_r, pid_l):
-        
-    x, y, theta = robot.get_position_and_angle()
-
-    info_lines = [
-        "---------------------------------------------------------------------",
-        f"Left Wheel Velocity:         {robot.get_left_wheel_velocity():.2f} m/s",
-        f"Left Wheel Velocity target:  {left_wheel_velocity:.2f} m/s",
-        f"P: {pid_l.P:6.2f}    I: {pid_l.I:6.2f}    D: {pid_l.D:6.2f}    PID: {pid_l.PID:6.2f}",
-        "",
-        f"Right Wheel Velocity:        {robot.get_right_wheel_velocity():.2f} m/s",
-        f"Right Wheel Velocity target: {right_wheel_velocity:.2f} m/s",
-        f"P: {pid_r.P:6.2f}    I: {pid_r.I:6.2f}    D: {pid_r.D:6.2f}    PID: {pid_r.PID:6.2f}",
-        "",
-        f"Base_Speed:                  {base_speed:.2f} m/s",
-        f"angle:                       {math.degrees(theta):.2f} °",
-        f"angle_setpoint:              {math.degrees(angle_setpoint):.2f} °",
-        f"angle_control:               {angle_control:.2f}"
-    ]
-
-    print("\033[H\033[J", end="")  # ANSI escape code to clear the screen
-    print("\n".join(info_lines))
 
 def main():
     parser = argparse.ArgumentParser(description="Robot control script")
