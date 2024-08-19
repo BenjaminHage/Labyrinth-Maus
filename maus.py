387de4f2af89bfc8bcf01ca6131b83f3016fb4cb
BenjaminHage <hagebenjamin@gmail.com>
2024-08-19 15:47:22 +0200

    Update maus.py
    argparse für sensor parameter file hinzugefügt

--- maus.py
+++ maus.py
@@ -1,15 +1,10 @@
+import argparse
 import time
 import math
 import numpy as np
-
 from controllers import PIDController
 import io_management as io
 from robot import DifferentialDriveRobot as Robot
-
-        
-################################################################################################################
-################################################################################################################
-
 
 def print_terminal(robot, left_wheel_velocity, right_wheel_velocity, base_speed,
                    angle_setpoint, angle_control, pid_r, pid_l):
@@ -34,27 +29,29 @@ def print_terminal(robot, left_wheel_velocity, right_wheel_velocity, base_speed,
 
     print("\033[H\033[J", end="")  # ANSI escape code to clear the screen
     print("\n".join(info_lines))
-    
-
-###############################################################################################################
-
 
 def main():
-    
-    speed_pid_left = PIDController(kp=450, ki=4000, kd=0, i_max = 550,d_max= 70, i_min = 0, pid_min = 0) #4500 16000	
-    speed_pid_right = PIDController(kp=450, ki=4000, kd=0, i_max = 550, d_max= 70, i_min = 0, pid_min = 0)
+    parser = argparse.ArgumentParser(description="Robot control script")
+    parser.add_argument('-f', '--filename', type=str, default=None, help='Name der Datei mit den parametern für die Distanzmessung')
+    args = parser.parse_args()
+
+    speed_pid_left = PIDController(kp=450, ki=4000, kd=0, i_max=550, d_max=70, i_min=0, pid_min=0)
+    speed_pid_right = PIDController(kp=450, ki=4000, kd=0, i_max=550, d_max=70, i_min=0, pid_min=0)
     speed_pid_left.set_integral(0.00000000000000001)
     speed_pid_right.set_integral(0.00000000000000001)
     
     angle_pid = PIDController(kp=0.2, ki=0.000, kd=0)
-    
-    robot = Robot()
-    
+
+    # Initialisiere den Roboter mit dem gegebenen Dateinamen (falls angegeben)
+    if args.filename:
+        robot = Robot(param_file=args.filename)
+    else:
+        robot = Robot()
+
     out = io.OutputManager()
     out.start_console_output()
     #out.start_rt_plot()
 
-    
     angle_setpoint = 0
     base_speed = 0
     close = False
@@ -62,7 +59,6 @@ def main():
     last_time = time.monotonic()
     start_Time = time.monotonic()
     duration = np.inf
-    
 
     try:
         while True:
@@ -77,12 +73,10 @@ def main():
             
             angle_setpoint, base_speed, close = io.handle_user_input(angle_setpoint, base_speed, close)
 
-
             # PID controller to adjust wheel velocities
-            
             angle_control = angle_pid.update(angle_setpoint, theta, time_step)
-            left_wheel_velocity = base_speed - angle_setpoint#- angle_control
-            right_wheel_velocity = base_speed + angle_setpoint#+ angle_control
+            left_wheel_velocity = base_speed - angle_setpoint  # - angle_control
+            right_wheel_velocity = base_speed + angle_setpoint  # + angle_control
             
             if right_wheel_velocity == 0:
                 speed_pid_right.set_integral(0.00000000000000001)
@@ -94,39 +88,29 @@ def main():
             left_motor_control = speed_pid_left.update(abs(left_wheel_velocity), robot.get_left_wheel_velocity(), time_step)
             robot.set_left_motor(int(left_motor_control * np.sign(left_wheel_velocity)))
             
-            
             robot.state_estimate(left_wheel_velocity, right_wheel_velocity)
 
-             
             out.update_console_output(robot, left_wheel_velocity, right_wheel_velocity, base_speed, angle_setpoint, angle_control, speed_pid_right, speed_pid_left)
             out.update_plot(current_time, 
-                                robot.get_left_wheel_velocity(), 
-                                robot.get_right_wheel_velocity(), 
-                                left_wheel_velocity, 
-                                right_wheel_velocity)
-            
+                            robot.get_left_wheel_velocity(), 
+                            robot.get_right_wheel_velocity(), 
+                            left_wheel_velocity, 
+                            right_wheel_velocity)
 
     except KeyboardInterrupt:
         print("main loop closed by keyboardInterupt")
         
-    
     try:
         print("Messung beendet.")
-       
         out.stop_console_output()
-        
         out.aktivate_final_batch_plot()
         out.aktivate_final_rt_plot()
         out.show_final_plots(robot)
         
-    
     except KeyboardInterrupt:
         print("Plot Closed by keyboardInterupt")
         
     print("\nEnd of Program")    
-        
-
 
 if __name__ == "__main__":
     main()
-
