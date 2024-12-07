bc1c0d4f62dc8b151e15abd3879d0f6a53fa95db
BenjaminHage <hagebenjamin@gmail.com>
2024-12-07 16:48:11 +0100

    Update maus.py

--- maus.py
+++ maus.py
@@ -59,7 +59,7 @@ def main():
     
     point_distance_pid = PIDController(kp=-1.2, ki=-0.0, kd=0, i_minmax=100, d_max=70, pid_minmax=0.5)
     wall_distance_pid = PIDController(kp=0.5, ki=0.000, kd=0.35, d_minmax=0.029, i_minmax=1, pid_minmax = 12, pid_min=-0.2)
-    diag_wall_distance_pid = PIDController(kp=0.5, ki=0.000, kd=0.35, d_minmax=0.029, i_minmax=1, pid_minmax = 10, pid_min=-0.02)
+    diag_wall_distance_pid = PIDController(kp=0.5, ki=0.000, kd=0.35, d_minmax=0.029, i_minmax=1, pid_minmax = 10, pid_min=-0.2)
     esc = ESCController(dither_frequency, dither_amplitude, learning_rate)
     
     auto = AutonomousController(angle_pid, wall_distance_pid, diag_wall_distance_pid, point_distance_pid, esc, init_base_speed,
